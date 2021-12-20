e41c5813dda9aadf9fb47b83270bae3c9b346e75
Muhammed <muhammed.bakijev@gmail.com>
2021-12-20 18:31:08 +0100

    refactor: add serilog

--- Bazon.Backend.Api/Program.cs
+++ Bazon.Backend.Api/Program.cs
@@ -1,35 +1,53 @@
 using Bazon.Backend.Api.DbModels;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().
+    WriteTo.Console().
+    CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console());
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMvc();
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<BazonContext>(opt =>
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("BazonDb")));
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddMvc();
+    builder.Services.AddEntityFrameworkNpgsql().AddDbContext<BazonContext>(opt =>
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("BazonDb")));
 
-var app = builder.Build();
-
+    var app = builder.Build();
+    app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseRouting();
+    app.UseAuthorization();
+
+    app.UseEndpoints(ep => { ep.MapControllers(); });
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-app.UseAuthorization();
-
-app.UseEndpoints(ep =>
+catch (Exception e)
 {
-    ep.MapControllers();
-});
-
-app.Run();
+    Log.Fatal("Application is unable to stat up: {e}", e);
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
